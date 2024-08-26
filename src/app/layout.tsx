29d7fb71011041856ac2a048ba3e04325ffb49c1
Muhammad Husnain <aimahusnain@gmail.com>
2024-08-26 22:57:42 +0500

    Favicon changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <meta name='theme-color' content="#f7bf45" />
+      <meta name='theme-color' content="#fff" />
       </head>
       <body className={inter.className}>
         <Navbar />
