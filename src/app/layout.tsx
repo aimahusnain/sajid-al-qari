9a6b50bf9a4eee654d667b1592f0edb85640dd63
Muhammad Husnain <aimahusnain@gmail.com>
2024-08-29 17:45:24 +0500

    GTM url changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,11 +27,11 @@ export default function RootLayout({
       <head>
         <meta name="theme-color" content="#fff" />
       </head>
-      <GoogleTagManager gtmId="GTM-5DNKHPQ9" />
+      <GoogleTagManager gtmId="GTM-53LH73MC" />
       <body className={inter.className}>
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5DNKHPQ9"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-53LH73MC"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
