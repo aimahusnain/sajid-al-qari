ad2bc9173c07852308f3bb96d07b54d66b32546f
Muhammad Husnain <aimahusnain@gmail.com>
2024-08-29 12:37:41 +0500

    Google tag manager code added as per the requirments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/footer";
 import FloatingContactButton from "@/components/FloatingContact";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,14 +25,46 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <meta name='theme-color' content="#fff" />
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),
+                  dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5DNKHPQ9');
+          `,
+          }}
+        />
+
+        <meta name="theme-color" content="#fff" />
       </head>
       <body className={inter.className}>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-5DNKHPQ9"
+              height="0"
+              width="0"
+              style="display:none;visibility:hidden"
+            ></iframe>
+          `,
+          }}
+        />
         <Navbar />
         <FloatingContactButton />
         {children}
         <Footer />
-        </body>
+      </body>
     </html>
   );
 }
