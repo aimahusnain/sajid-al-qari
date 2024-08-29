9153192b6759f70667e56b516009ccfc6ef34e71
Muhammad Husnain <aimahusnain@gmail.com>
2024-08-29 17:07:58 +0500

    Google Tag Manager added v1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
+import FloatingContactButton from "@/components/FloatingContact";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/footer";
+import { GoogleTagManager } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/footer";
-import FloatingContactButton from "@/components/FloatingContact";
-import Script from "next/script";
-import { GoogleTagManager } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,45 +25,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager */}
-        {/* <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            (function(w,d,s,l,i){
-              w[l]=w[l]||[];
-              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-              var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),
-                  dl=l!='dataLayer'?'&l='+l:'';
-              j.async=true;
-              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-              f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5DNKHPQ9');
-          `,
-          }}
-        /> */}
-
         <meta name="theme-color" content="#fff" />
       </head>
+      <GoogleTagManager gtmId="GTM-TN3BS5Q7" />
       <body className={inter.className}>
-        {/* <noscript
-          dangerouslySetInnerHTML={{
-            __html: `
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-5DNKHPQ9"
-              height="0"
-              width="0"
-              style="display:none;visibility:hidden"
-            ></iframe>
-          `,
-          }}
-        /> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TN3BS5Q7"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Navbar />
         <FloatingContactButton />
         {children}
-        <GoogleTagManager gtmId="GTM-5DNKHPQ9" />
         <Footer />
       </body>
     </html>
